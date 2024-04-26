729bca46d71dcd92d7bd9e0eed221b240a70eaef
Shrasti Arjariya <135357296+Shrasti321@users.noreply.github.com>
2024-04-26 13:34:40 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,12 +1,19 @@
 def smallest_missing_positive_integer(nums: List[int]) -> int:
-    """
-    Implement the function smallest_missing_positive_integer 
-    using the provided smallest_missing_positive_integer function 
-    to find the smallest missing positive integer in the given list.
-
-    """
-    pass
-
+    n=max(nums)
+    if n<1:
+        return 1
+    if len(nums) == 1:
+        return 2 if nums[0] == 1 else 1
+    l = [0]*n
+    for i in range(len(nums)):
+        if nums[i]>0:
+            if l[nums[i]-1] != 1:
+                l[nums[i]] = 1
+    for i in range(len(l)):
+        if l[i] == 0:
+            return i+1
+    return i+2
+    
 
 
 
